e124166f65e16e2899dab6ce753b66ea1a315b05
Tedford Johnson <dev@tedfordjohnson.com>
2025-12-05 23:56:58 -0500

    day 3 - part 2

--- crates/day3/src/lib.rs
+++ crates/day3/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::min;
-
 fn parse(input: &Vec<String>) -> Vec<Vec<i32>> {
     input
         .iter()
@@ -35,10 +33,10 @@ fn find_largest_2_joltage(batteries: &Vec<i32>) -> i64 {
 }
 
 fn find_largest_12_joltage(batteries: &Vec<i32>) -> i64 {
-    let mut active = vec![0; 12];
+    let mut active = batteries[batteries.len() - 12..].to_vec();
     active[0] = batteries[0];
-    let mut index = 0;
-    let mut index2 = batteries.len() - active.len() + 1;
+    let mut index = 0; // index of the last assigned battery
+    let mut index2 = batteries.len() - active.len() + 1; // index of the first remainder
 
     for i in 1..index2 {
         if batteries[i] > active[0] {
@@ -49,28 +47,31 @@ fn find_largest_12_joltage(batteries: &Vec<i32>) -> i64 {
             break;
         }
     }
-    index += 1;
     println!("{} {:?}", index, batteries);
+    index += 1;
 
-    // grab initial values
-    for i in 1..active.len() {
-        active[i] = batteries[index2 + i-1];
-    }
-
-    let max = batteries.len();
     // compact the number range
     for digit in 1..active.len() {
-        for i in index..index2 {
+        let mut first_match = 0;
+        let mut new_index = 0;
+
+        for i in index..=index2 {
+            if batteries[i] == active[digit] && first_match == 0 {
+                first_match = i;
+            }
             if batteries[i] > active[digit] {
                 active[digit] = batteries[i];
-                index = i + 1;
-                index2 = min(index2 + digit, max);
-                if active[digit] == 9 {
-                    break;
-                }
+                new_index = i;
             }
         }
-        if index >= max {
+
+        if new_index > 0 {
+            index = new_index + 1;
+            index2 += 1;
+        } else if first_match > 0 {
+            index = first_match + 1;
+            index2 += 1;
+        } else {
             break;
         }
     }
@@ -80,6 +81,7 @@ fn find_largest_12_joltage(batteries: &Vec<i32>) -> i64 {
         .map(|n| n.to_string())
         .collect::<Vec<_>>()
         .join("");
+    println!("{}", s);
     s.parse::<i64>().unwrap()
 }
 
@@ -92,7 +94,9 @@ pub fn part1(input: &Vec<String>) -> i64 {
     joltages.iter().sum()
 }
 
-// 173804603992943 too low
+
+// 173843907657110 too low
+// 173960689460215 too high
 pub fn part2(input: &Vec<String>) -> i64 {
     let banks = parse(input);
     let joltages = banks
@@ -126,6 +130,20 @@ mod tests {
         let sample = get_sample();
         let result = part2(&sample);
         assert_eq!(result, 3121910778619);
+    }
+
+
+    #[test]
+    fn nine_heavy() {
+        let sample = vec!["9847555865827676657688569759758758555445794968796565767687569857578579948785658658585576858447769967".to_string()];
+        let result = part2(&sample);
+        assert_eq!(result, 999999999997);
+    }
+    #[test]
+    fn mixed_compaction() {
+        let sample = vec!["4238131312372266238322323992851888128123122242282434742712328963325226342472212242762454422754113423".to_string()];
+        let result = part2(&sample);
+        assert_eq!(result, 999777543423);
     }
 
     #[test]
